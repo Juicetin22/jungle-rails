3ac744de5528d3406524d4d897397fdb015c1f71
Justin Huang <justin.2260@gmail.com>
2022-02-03 14:57:00 -0500

    add @categories instance variable to render category buttons for each category

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   def show
     @number_of_products = Product.count(:id)
     @number_of_categories = Category.count(:id)
+    @categories = Category.all
+    p @categories[0][:name]
   end
 end
