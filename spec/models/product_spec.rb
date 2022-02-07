d1dba3af11928483626e197f5b56c00e3b89767b
Justin Huang <justin.2260@gmail.com>
2022-02-07 16:58:57 -0500

    create test that ensures product is saved successfully after inputting the four required fields

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it 'should successfully save when all four required input fields are set' do
+      @category = Category.create(name: "moomoo")
+      @product = Product.new
+      @product.name = "plop"
+      @product.price = 22
+      @product.quantity = 101
+      @product.category_id = @category.id
+      @product.save
+
+      puts "---------------------------"
+      puts @product.errors.messages
+      puts "---------------------------"
+      expect(@product.save).to be true
+      # expect(@product.errors.messages[:category]).to include "can't be blank"
+    end
   end
 end
