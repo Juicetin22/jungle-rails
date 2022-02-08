9fec00dabfc00ce8ad14c3f1cfdac9c68259cde3
Justin Huang <justin.2260@gmail.com>
2022-02-08 12:27:55 -0500

    update authenticate_with_credentials to include edge case for trailing whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true, length: { minimum: 5 }
 
   def self.authenticate_with_credentials(email, password)
-    @user = User.find_by_email(email)
+    modified_email = email.strip
+    @user = User.find_by_email(modified_email)
     # if user exists AND the password entered is correct, return @user to session controller
     if @user && @user.authenticate(password)
       @user
