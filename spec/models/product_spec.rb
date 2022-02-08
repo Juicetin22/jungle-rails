b40c6d6d0b52654cb42368914f8064c0d72a4aee
Justin Huang <justin.2260@gmail.com>
2022-02-07 19:08:27 -0500

    create test that prevents saving successfully when name is not present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'should successfully save when all four required input fields are set' do
+      # create a category in database to use to test product
       @category = Category.create(name: "moomoo")
       @product = Product.new
       @product.name = "plop"
@@ -11,11 +12,29 @@ RSpec.describe Product, type: :model do
       @product.category_id = @category.id
       @product.save
 
+      # error message object should be empty
       puts "---------------------------"
       puts @product.errors.messages
       puts "---------------------------"
       expect(@product.save).to be true
       # expect(@product.errors.messages[:category]).to include "can't be blank"
+    end
+
+    it 'should prevent saving the product info when the name is not present' do
+      @category = Category.create(name: "moop")
+      @product = Product.new
+      @product.name = nil
+      @product.price_cents = 222
+      @product.quantity = 1010
+      @product.category_id = @category.id
+      @product.save
+
+      puts "---------------------------"
+      puts @product.errors.messages
+      puts @product.errors.full_messages
+      puts "---------------------------"
+      expect(@product.save).to be false
+      expect(@product.errors.messages[:name]).to include "can't be blank"
     end
   end
 end
