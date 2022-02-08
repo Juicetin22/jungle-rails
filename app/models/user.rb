faebe022b043e0a73a349026b82a4c8b41b6fa4f
Justin Huang <justin.2260@gmail.com>
2022-02-08 12:04:09 -0500

    create authenticate_with_credneitals method as class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password_confirmation, presence: true, length: { minimum: 5 }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    # if user exists AND the password entered is correct, return @user to session controller
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
   
 end
