263dade0b5049a6272982ef8d2b62aee089741c2
Justin Huang <justin.2260@gmail.com>
2022-02-07 23:19:54 -0500

    create test that tests if last name field is empty

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,5 +66,24 @@ RSpec.describe User, type: :model do
       expect(@user.save).to be false
       expect(@user.errors.messages[:first_name]).to include "can't be blank"
     end
+
+    it 'should not save if first name field is empty' do
+      @user = User.new
+      @user.first_name = "Moo"
+      @user.last_name = nil
+      @user.email = "moo@boo.com"
+      @user.password = "mooboo"
+      @user.password_confirmation = "mooboo"
+      
+      @user.save
+
+      puts "------------user test---------------"
+      puts @user.errors.messages
+      puts @user.errors.full_messages
+      puts "---------------------------"
+
+      expect(@user.save).to be false
+      expect(@user.errors.messages[:last_name]).to include "can't be blank"
+    end
   end
 end
