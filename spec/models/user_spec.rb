1eaf66c3310ed3b1b9be58388fdb1f8d97f52f1c
Justin Huang <justin.2260@gmail.com>
2022-02-07 23:14:04 -0500

    create test to prevent creating a new user if email is not unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,32 @@ RSpec.describe User, type: :model do
       expect(@user.errors.messages.length).to be 0
     end
 
+    it 'should not save if email is not unique' do
+      @testuser = User.create(first_name: "George", last_name: "Bob", email: "george@bob.com", password: "georgebob", password_confirmation: "georgebob")
+      @testuser2 = User.create(first_name: "Jeorg", last_name: "Bog", email: "jeorg@bog.com", password: "jeorgbog", password_confirmation: "jeorgbog")
+      @user = User.new
+      @user.first_name = "Moo"
+      @user.last_name = "Boo"
+      @user.email = "george@boB.com"
+      @user.password = "mooboo"
+      @user.password_confirmation = "mooboo"
+      
+      @user.save
+
+      puts "------------user test---------------"
+      puts @user.errors.messages
+      puts @user.errors.full_messages
+      puts "---------------------------"
+
+      expect(@user.save).to be false
+      # expect(@user.password == @user.password_confirmation).to be true
+      
+      list_of_emails = []
+      User.all.each do |user|
+        list_of_emails << user.email
+      end
+      expect(list_of_emails).not_to include @user.email
+    end
   
   end
 end
